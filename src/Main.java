e111b7b561eb2eab8fe5d840d6b5f35abbbec2aa
kirillWaste <kiriliys077@gmail.com>
2023-09-12 15:08:54 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+import java.math.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите n");
+        int n = sc.nextInt();
+        System.out.println("Введите x");
+        double x = sc.nextDouble();
+        System.out.println("Введите y");
+        double y = sc.nextDouble();
+        double a = Math.sqrt(((x+y)/Math.log10(x*x))*n);
+        System.out.println(a);
+        }
+
+    }
